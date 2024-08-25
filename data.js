bc4e3dd8d4746cc337e37c6aef45f04d508206b0
Dmytro Tkachuk <d.tkachuk@pinglestudio.com>
2024-08-25 14:33:05 +0300

    coridors

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-ydxj0009-panorama",
-      "name": "YDXJ0009 Panorama",
+      "id": "0-",
+      "name": "Екстерєр",
       "levels": [
         {
           "tileSize": 256,
@@ -24,23 +24,155 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -1.7598758878361256,
+        "pitch": 0.0033842606015621612,
+        "fov": 1.4658855057614426
       },
       "linkHotspots": [
         {
-          "yaw": -1.6701077159394266,
-          "pitch": 0.008925778213837887,
+          "yaw": -1.6330562227864718,
+          "pitch": 0.02494616196380761,
           "rotation": 0,
-          "target": "1-ydxj0035-panorama"
+          "target": "1-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-ydxj0035-panorama",
-      "name": "YDXJ0035 Panorama",
+      "id": "1-",
+      "name": "Вхід",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9837124178424492,
+        "pitch": -0.1861459218116579,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9806717881609899,
+          "pitch": 0.025212512445744295,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.16983049748275292,
+          "pitch": -0.00407698166300996,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Вестибюль",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.0733507179034163,
+        "pitch": 0.03543107986269867,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8629829632337742,
+          "pitch": 0.19248488223902882,
+          "rotation": 0,
+          "target": "8--1-"
+        },
+        {
+          "yaw": 0.7802223790796177,
+          "pitch": 0.003326238968497819,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---2-",
+      "name": "Західне крило 2 поверх",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.236802294492625,
+        "pitch": 0.13919837900152388,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0978044807464666,
+          "pitch": 0.08695443985077844,
+          "rotation": 0,
+          "target": "4--2-"
+        },
+        {
+          "yaw": 1.4991409675323206,
+          "pitch": 0.14318611749661336,
+          "rotation": 0,
+          "target": "7---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--2-",
+      "name": "Коридор 2 поверх",
       "levels": [
         {
           "tileSize": 256,
@@ -68,19 +200,207 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.2702030297773277,
-          "pitch": 0.012269309386283567,
+          "yaw": 1.4838981010469778,
+          "pitch": 0.037880350850439726,
           "rotation": 0,
-          "target": "0-ydxj0009-panorama"
+          "target": "3---2-"
+        },
+        {
+          "yaw": -1.691782020245057,
+          "pitch": 0.03327984379808768,
+          "rotation": 0,
+          "target": "5---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---2-",
+      "name": "Східне крило 2 поверх",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1623977724348151,
+          "pitch": 0.10308695711055549,
+          "rotation": 0,
+          "target": "4--2-"
+        },
+        {
+          "yaw": 2.205012438884051,
+          "pitch": 0.1387607398606061,
+          "rotation": 0,
+          "target": "6---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---1-",
+      "name": "Східне крило 1 поверх",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.15664789091171727,
+        "pitch": -0.060741110837026824,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3611527112280104,
+          "pitch": 0.09312167789937575,
+          "rotation": 0,
+          "target": "8--1-"
+        },
+        {
+          "yaw": 1.9527583287133208,
+          "pitch": -0.1475206710286585,
+          "rotation": 0,
+          "target": "5---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7---1-",
+      "name": "Західне крило 1 поверх",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1009160144267014,
+        "pitch": 0.06785840444682911,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7636068040172654,
+          "pitch": -0.19548985798233076,
+          "rotation": 0,
+          "target": "3---2-"
+        },
+        {
+          "yaw": -1.8652809040491949,
+          "pitch": 0.004338865162777594,
+          "rotation": 0,
+          "target": "8--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8--1-",
+      "name": "Коридор 1 поверх",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.68993480934601,
+        "pitch": 0.2140132259967924,
+        "fov": 1.4658855057614426
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.30453935700546,
+          "pitch": 0.22345302058495697,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.0494265365301825,
+          "pitch": 0.049885504537806824,
+          "rotation": 0,
+          "target": "7---1-"
+        },
+        {
+          "yaw": 0.08404123636800342,
+          "pitch": 0.04323500946783554,
+          "rotation": 0,
+          "target": "6---1-"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Шляхівська школа",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
